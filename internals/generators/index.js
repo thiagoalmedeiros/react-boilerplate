6b7e299897d05bdc3fa10291c8446d5a9c5a6e21
Thiago Medeiros <thiagomedeiros@MacBook-Pro-de-Thiago.local>
2017-03-19 23:20:17 -0300

    Creating shared components generator

--- internals/generators/index.js
+++ internals/generators/index.js
@@ -7,12 +7,14 @@
 const fs = require('fs');
 const path = require('path');
 const componentGenerator = require('./component/index.js');
+const componentSharedGenerator = require('./shared/index.js');
 const containerGenerator = require('./container/index.js');
 const routeGenerator = require('./route/index.js');
 const languageGenerator = require('./language/index.js');
 
 module.exports = (plop) => {
   plop.setGenerator('component', componentGenerator);
+  plop.setGenerator('sharedComponent', componentSharedGenerator);
   plop.setGenerator('container', containerGenerator);
   plop.setGenerator('route', routeGenerator);
   plop.setGenerator('language', languageGenerator);
@@ -21,7 +23,12 @@ module.exports = (plop) => {
       fs.accessSync(path.join(__dirname, `../../app/containers/${comp}`), fs.F_OK);
       return `containers/${comp}`;
     } catch (e) {
-      return `components/${comp}`;
+      try{
+        fs.accessSync(path.join(__dirname, `../../app/components/${comp}`), fs.F_OK);
+        return `components/${comp}`;
+      }catch (e) {
+        return `components/shared/${comp}`;
+      }
     }
   });
   plop.addHelper('curly', (object, open) => (open ? '{' : '}'));
--- internals/generators/utils/componentExists.js
+++ internals/generators/utils/componentExists.js
@@ -7,8 +7,9 @@
 const fs = require('fs');
 const path = require('path');
 const pageComponents = fs.readdirSync(path.join(__dirname, '../../../app/components'));
+const pageComponentShareds = fs.readdirSync(path.join(__dirname, '../../../app/components/shared'));
 const pageContainers = fs.readdirSync(path.join(__dirname, '../../../app/containers'));
-const components = pageComponents.concat(pageContainers);
+const components = pageComponents.concat(pageContainers).concat(pageComponentShareds);
 
 function componentExists(comp) {
   return components.indexOf(comp) >= 0;
